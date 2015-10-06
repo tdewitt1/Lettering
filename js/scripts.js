abbaa37d28b01b44730dedde11234873577aeabc
tdewitt1 <taylordewitt@eagles.ewu.edu>
2015-10-06 14:32:21 -0700

    End of Class Push

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+//My Scripts
+// Default Show No Name
+// Get & Store First Name
+//var firstName = prompt();
+//console.log(firstName);
+// Get & Store Last Name
+//var lastName = prompt();
+//console.log(lastName);
+// Display First & Last Name
+//$('.first').text(firstName);
+//$('.last').text(lastName);
+//Turn on Lettering
+$('h1').lettering();
